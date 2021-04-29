1a8a48ce92683d7be1e7a0fe4cd9038439593a5b
Rafael Lopez <raflopez1@gmail.com>
2021-04-29 13:03:46 +0000

    shouldn't attempt detach on non bcache devices

--- cmd/detach.go
+++ cmd/detach.go
@@ -20,23 +20,18 @@ func (b *bcache_devs) RunDetach (cdev string, bdev string) {
   var writepath string = SYSFS_BLOCK_ROOT
   var x bool
   var y bcache_cdev
+  var z bcache_bdev
   x, y = b.IsCDevice(cdev)
   if ! x {
     fmt.Println(cdev, "is not a registered cache device.")
     return
   }
-  for _, dev := range b.bdevs {
-    if dev.BackingDev == bdev ||
-      dev.ShortName == bdev ||
-      dev.BcacheDev == bdev {
-      if dev.CacheDev == "(none attached)" {
-        fmt.Println("No cache device currently attached to", bdev)
-        return
-      }
-      writepath = writepath+dev.ShortName+`/bcache/detach`
-      break
-    }
+  x, z = b.IsBDevice(bdev)
+  if ! x {
+    fmt.Println(bdev, "is not a registered backing device.")
+    return
   }
+  writepath = writepath+z.ShortName+`/bcache/detach`
   ioutil.WriteFile(writepath, []byte(y.UUID), 0)
   fmt.Println("Detached cache dev", cdev, "from "+bdev)
 }
