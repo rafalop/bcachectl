436be7d85b5bac9de2e5ecee2b771da79967a78c
Rafael Lopez <rafael.lopez@monash.edu>
2021-07-13 14:19:25 +1000

    changed show command to accept exactly 1 arg

--- cmd/show.go
+++ cmd/show.go
@@ -9,7 +9,7 @@ var showCmd = &cobra.Command{
   Use:   "show {device}",
   Short: "Show detailed information about a bcache device",
   Long: "If a cache or backing device is supplied, info will be displayed for the bcache device which it is a member of",
-  Args: cobra.MaximumNArgs(1),
+  Args: cobra.ExactArgs(1),
   Run: func(cmd *cobra.Command, args []string) {
     all := allDevs()
     all.RunShow(Format, args[0])
